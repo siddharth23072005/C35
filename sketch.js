7fbb060412a50d8751d4056068e0167ef2a333e3
siddharth23072005 <64962396+siddharth23072005@users.noreply.github.com>
2020-07-16 07:33:15 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var canvas, database;
+var gameState = 0;
+var playerCount;
+var form, game,player;
+
+function setup(){
+  database = firebase.database();
+  
+ canvas =  createCanvas(400,400);
+ game  = new Game();
+ game.getState();
+ game.start();
+  
+
+  
+}
+
+function draw(){
+ 
+    
+}
+
